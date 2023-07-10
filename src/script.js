f6f498c4278b1d2076cbbb78d4771b768dad3e12
myselfakashagarwal <myselfakashagarwal@gmail.com>
2023-07-11 04:09:32 +0530

    restricted@\!desktop

--- src/script.js
+++ src/script.js
@@ -7,12 +7,6 @@ const textureLoader = new THREE.TextureLoader()
 import device from "current-device"
 
 
-
-
-
-
-
-
 //@ constants for measurement and other 
 
 const sizes = {
@@ -24,13 +18,11 @@ if(device.type == 'desktop')
     sizes.width = window.innerWidth*0.93;
 }
 
-
-
 //@THREE.js building and all 3d related stuff 
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.001, 10000)
-camera.position.set(0,0,1)
+camera.position.set(0,0,7)
 scene.add(camera)    
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
@@ -40,16 +32,24 @@ controls.maxPolarAngle = Math.PI / 2.8
 
 const gltf_loader=new GLTFLoader()
 var object;
+gltf_loader.load('./sculpture/scene.gltf',(gltf)=>{ object=gltf.scene;object.scale.set(0.13,0.13,0.13);object.position.set(0,-4,0);object.rotation.y=3;scene.add(object)}) 
    
 const renderer = new THREE.WebGLRenderer({canvas: canvas})
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(window.devicePixelRatio)
 
+const wireframeGeometry = new THREE.BoxBufferGeometry(15,32,16)
+const wireframeMaterial = new THREE.MeshNormalMaterial({wireframe:true})
+wireframeMaterial.side = THREE.DoubleSide
+const wireframe = new THREE.Mesh(wireframeGeometry,wireframeMaterial)
+scene.add(wireframe)
 
 //Animate
+const time = clock.getElapsedTime()
 const tick = () =>
 {
-    
+    const time = clock.getElapsedTime()
+    wireframe.rotation.y= 1*time
     // Update controls
     controls.update()
     // Render
@@ -60,24 +60,20 @@ const tick = () =>
 tick()
 
 const homeDivHeading = document.getElementById('homeDivHeading')
-
+const sin = document.querySelectorAll('.sin');
+const mobile = document.getElementById('mobile')
 
 
 if(device.type != "desktop")
 {
-    gltf_loader.load('./sculpture/scene.gltf',(gltf)=>{ object=gltf.scene;object.scale.set(0.11,0.11,0.11);object.position.set(0,-6,0);object.rotation.y=3;scene.add(object)});
-    const introHeadingKeyword = document.getElementById('introHeadingKeyWord') 
-    introHeadingKeyword.style.fontSize = "80px"
-    canvas.style.maxWidth = "95%"
-
+   const allStuff = document.getElementById('allStuff')
+   allStuff.style.display = "none"
+   allStuff.remove();
 }
 else
 {
-    gltf_loader.load('./sculpture/scene.gltf',(gltf)=>{ object=gltf.scene;object.scale.set(0.13,0.13,0.13);object.position.set(0,-4,0);object.rotation.y=3;scene.add(object)}) 
-    homeDivHeading.style.fontSize = "110px"
-    homeDivHeading.style.textAlign = "left"
-    homeDivHeading.style.marginLeft = "5%"
-
+    mobile.style.display = "none"
+    mobile.remove();
 }
 
 /////////
@@ -93,5 +89,5 @@ const observer = new IntersectionObserver ((entries) => {
     });
     });
 
-const sin = document.querySelectorAll('.sin');
+
 sin.forEach((el) => observer .observe (el));
